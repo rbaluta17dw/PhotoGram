3b93db9ef5337cec4acb6fe801b3cd13818bae7f
Maurii19 <mdiazar17dw@ikzubirimanteo.com>
2018-04-24 08:11:40 +0200

    modelos y conector

---
+++ src/config/Conector.java
@@ -0,0 +1,30 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector extends Config {
+	
+	protected Connection conexion;
+
+	
+	 protected Conector(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			this.conexion = DriverManager.getConnection("jdbc:mysql://" + HOST + "/" + BBDD, USERNAME, PASSWORD);
+		}catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConexion(){
+		return conexion;
+	}
+	public void setConexion(Connection conexion){
+		this.conexion = conexion;
+	}
+}
+
